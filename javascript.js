72c82ccf163d20de156bf2944375300b025c88d7
AllV64 <114334244+AllV64@users.noreply.github.com>
2023-09-12 09:02:27 +0200

    Refactor code for readability

--- javascript.js
+++ javascript.js
@@ -12,7 +12,60 @@ function createGrid(size){
         }
     }
     return 'updated';
-}
+};
+
+let squareRoot = 16;
+createGrid(squareRoot);
+let cells = container.querySelectorAll('div.row > div');
+
+function cellHover(){
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            addRandomColor(cell);
+            addBrightness(cell);
+        });
+    });
+};
+cellHover();
+
+function addRandomColor(cell){
+    if (cell.style.background == ""){
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        cell.style.cssText = `background: rgb(${red}, ${green}, ${blue})`; 
+    }
+};
+
+function addBrightness(cell){
+    if (cell.style.filter == ''){
+        cell.style.filter = 'brightness(0.1)';
+    } else if (cell.style.filter == 'brightness(1)'){
+        cell.style.filter == cell.style.filter;
+    } else {
+        let brightnessValue = cell.style.filter.slice(-4, -1);
+        cell.style.filter = `brightness(${parseFloat(brightnessValue) + 0.1})`;
+    }
+    
+};
+
+const buttonContainer = document.querySelector('#button-container');
+const invalidInput = document.createElement('div');
+invalidInput.style.cssText = 'color: red;';
+buttonContainer.appendChild(invalidInput);
+
+changeGridSize = document.querySelector('button');
+changeGridSize.addEventListener('click', () => {
+    container.innerHTML = '';
+    let userInput = prompt(`Type a number between 1 and 100`);
+    if (validateUserInput(userInput) == true) {
+        userInput = parseInt(userInput);
+        squareRoot = userInput;
+    };
+    createGrid(squareRoot);
+    cells = container.querySelectorAll('div.row > div');
+    cellHover();
+});
 
 function validateUserInput(userInput){
     if (isNaN(userInput)){
@@ -25,56 +78,5 @@ function validateUserInput(userInput){
         invalidInput.textContent = "";
         return true;
     }
-}
-
-let squareRoot = 16;
-createGrid(squareRoot);
-let cells = container.querySelectorAll('div.row > div');
-
-function cellHover(){
-    cells.forEach((cell) => {
-        cell.addEventListener('mouseover', () => {
-            addRandomColor(cell);
-            addBrightness(cell);
-        })
-    });
 };
-cellHover();
 
-const buttonContainer = document.querySelector('#button-container');
-const invalidInput = document.createElement('div');
-invalidInput.style.cssText = 'color: red;';
-buttonContainer.appendChild(invalidInput);
-changeGridSize = document.querySelector('button');
-changeGridSize.addEventListener('click', () => {
-    container.innerHTML = '';
-    let userInput = prompt(`Type a number between 1 and 100`);
-    if (validateUserInput(userInput) == true) {
-        userInput = parseInt(userInput);
-        squareRoot = userInput;
-    } ;
-    createGrid(squareRoot);
-    cells = container.querySelectorAll('div.row > div');
-    cellHover();
-})
-
-function addRandomColor(cell){
-    if (cell.style.background == ""){
-        let red = Math.floor(Math.random() * 256);
-        let green = Math.floor(Math.random() * 256);
-        let blue = Math.floor(Math.random() * 256);
-        cell.style.cssText = `background: rgb(${red}, ${green}, ${blue})`; 
-    }
-}
-
-function addBrightness(cell){
-    if (cell.style.filter == ''){
-        cell.style.filter = 'brightness(0.1)';
-    } else if (cell.style.filter == 'brightness(1)'){
-        cell.style.filter == cell.style.filter;
-    } else {
-        let brightnessValue = cell.style.filter.slice(-4, -1);
-        cell.style.filter = `brightness(${parseFloat(brightnessValue) + 0.1})`;
-    }
-    
-}
